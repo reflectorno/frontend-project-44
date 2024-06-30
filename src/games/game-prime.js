e8314f6e94b7118b157c38a3c271fe013542dccd
Sergei Kravchenko <reflectorno@gmail.com>
2024-06-30 14:43:45 +0300

    add game-prime.js

---
+++ src/games/game-prime.js
@@ -0,0 +1,25 @@
+import gameLogic from '../index.js';
+
+const getRandomNumber = (min = 2, max = 10) => Math.floor(Math.random() * (max - min + 1)) + min;
+const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (num) => {
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return 'no';
+    }
+  }
+  return 'yes';
+};
+
+const getGame = () => {
+  const question = getRandomNumber();
+  const correctAnswer = isPrime(question);
+  return [question, correctAnswer];
+};
+
+const gamePrime = () => {
+  gameLogic(task, getGame);
+};
+
+export default gamePrime;
