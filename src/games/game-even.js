b6350cb507bb483edd82112befec5811a0f38f4c
Sergei Kravchenko <reflectorno@gmail.com>
2024-06-30 14:43:07 +0300

    fix line break

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -1,7 +1,8 @@
 import gameLogic from '../index.js';
 
-const getRandomNumber = () => Math.floor(Math.random() * 100);
+const getRandomNumber = (min = 1, max = 10) => Math.floor(Math.random() * (max - min + 1)) + min;
 const isEven = (num) => num % 2 === 0;
+
 const task = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const getGame = () => {
