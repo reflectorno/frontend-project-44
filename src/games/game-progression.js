1fbe319fb00703cae1ad7e8da32c1cc99aa75e34
Sergei Kravchenko <reflectorno@gmail.com>
2024-07-03 14:41:27 +0300

    fix game-progression.js

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -3,15 +3,20 @@ import gameLogic from '../index.js';
 const getRandomNumber = (min = 0, max = 10) => Math.floor(Math.random() * (max - min + 1)) + min;
 const task = 'What number is missing in the progression?';
 
+const getProgression = (firstNum, progressionLength, stepOfProgression) => {
+  const result = [];
+  for (let i = 0; i < progressionLength; i += 1) {
+    result.push(firstNum + i * stepOfProgression);
+  }
+  return result;
+};
+
 const getGame = () => {
   const progressionLength = getRandomNumber(5, 10);
   const firstNum = getRandomNumber(0, 10);
   const stepOfProgression = getRandomNumber(1, 10);
   const missNum = getRandomNumber(0, progressionLength - 1);
-  const progression = [];
-  for (let i = 0; i < progressionLength; i += 1) {
-    progression.push(firstNum + i * stepOfProgression);
-  }
+  const progression = getProgression(firstNum, progressionLength, stepOfProgression);
   const correctAnswer = progression[missNum];
   progression[missNum] = '..';
   const question = progression.join(' ');
